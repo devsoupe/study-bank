10246888e4d254dca9425828044dde3ba52cee33
devsoupe <luvyum4e@gmail.com>
2023-08-08 13:36:56 +0900

    first commit

---
+++ src/main/java/shop/mtcoding/bank/StudyBankApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBankApplication.class, args);
+    }
+
+}
