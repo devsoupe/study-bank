fab51242ffe8bd9d9a65395e951810db9aed013d
devsoupe <luvyum4e@gmail.com>
2023-08-17 23:07:16 +0900

    회원가입 컨트롤러 정규표현식 실습2

--- src/test/java/shop/mtcoding/bank/regex/RegexTest.java
+++ src/test/java/shop/mtcoding/bank/regex/RegexTest.java
@@ -1,8 +1,11 @@
 package shop.mtcoding.bank.regex;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class RegexTest {
 
@@ -10,32 +13,42 @@ public class RegexTest {
     public void 한글만된다_test() throws Exception {
         String value = "한글";
         boolean result = Pattern.matches("^[가-힣]+$", value);
-        System.out.println("테스트 : " + result);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void 한글은안된다_test() throws Exception {
-
+        String value = "";
+        boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void 영어만된다_test() throws Exception {
-
+        String value = "abc";
+        boolean result = Pattern.matches("^[a-zA-Z]+$", value);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void 영어는안된다_test() throws Exception {
-
+        String value = "가22";
+        boolean result = Pattern.matches("^[^a-zA-Z]*$", value);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void 영어와숫자만된다_test() throws Exception {
-
+        String value = "ab12";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void 영어만되고_길이는최소2최대4이다_test() throws Exception {
-
+        String value = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
+        assertThat(result).isTrue();
     }
 
     // username, email, fullname
